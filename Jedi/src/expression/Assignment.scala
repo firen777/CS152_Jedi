5f157868f54a0300f62dbbe1708db1e84d23e1b9
firen777 <hivemindac1995@gmail.com>
2017-12-06 15:22:33 -0800

    Assignment drafted not tested

--- Jedi/src/expression/Assignment.scala
+++ Jedi/src/expression/Assignment.scala
@@ -2,15 +2,21 @@ package expression
 
 import context.Environment
 import value.Value
+import value.Notification
 
 
-/**vbl = update <br>
+/**id = exp <br>
  * extends expression.SpecialForm
  * @param vbl
  * @param update
  */
-case class Assignment(val vbl: Identifier, val update: Expression) extends SpecialForm {
+case class Assignment(val id: Identifier, val exp: Expression) extends SpecialForm {
+  /** Update id-exp pair in Environment table<br>
+   * @param env Environment table
+   * @return Value Notification.DONE
+   */
   override def execute(env:Environment):Value = {
-    null
+    env.put(id, exp.execute(env))
+    Notification.DONE
   }
 }
