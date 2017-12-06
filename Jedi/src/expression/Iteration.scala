2e65d418a0ddf1864a0987edd1fab746abeac294
firen777 <hivemindac1995@gmail.com>
2017-12-06 15:22:55 -0800

    Iteration drafted not tested

--- Jedi/src/expression/Iteration.scala
+++ Jedi/src/expression/Iteration.scala
@@ -2,6 +2,9 @@ package expression
 
 import context.Environment
 import value.Value
+import value.Boole
+import context.TypeException
+import value.Notification
 
 /**while (condition) {body}<br>
  * extends expression.SpecialForm
@@ -9,8 +12,22 @@ import value.Value
  * @param body
  */
 case class Iteration(val condition:Expression, val body:Expression) extends SpecialForm {
+  
+  
+  /**While loop<br>
+   * Throw TypeException if condition.execute is not Boole
+   * @throws TypeException
+   * @param env Environment table
+   * @return Value Notification.DONE
+   */
   override def execute(env:Environment):Value = {
-    null
+    if (!condition.execute(env).isInstanceOf[Boole])
+      throw new TypeException("Condition need to be of type Boole.")
+    else
+      while (condition.execute(env).asInstanceOf[Boole].value)
+        body.execute(env)
+    
+    Notification.DONE
   }
   
 }
